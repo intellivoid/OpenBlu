5efe302acc2a7038254daadab839e7f79c904dcc
Netkas <45276434+Netkas@users.noreply.github.com>
2020-01-18 22:29:37 -0300

    Added requirement for msqg

--- src/OpenBlu/Managers/UserSubscriptionManager.php
+++ src/OpenBlu/Managers/UserSubscriptionManager.php
@@ -4,6 +4,8 @@
     namespace OpenBlu\Managers;
 
 
+    use OpenBlu\Abstracts\UserSubscriptionStatus;
+    use OpenBlu\Objects\UserSubscription;
     use OpenBlu\OpenBlu;
 
     /**
@@ -24,5 +26,14 @@
         public function __construct(OpenBlu $openBlu)
         {
             $this->openBlu = $openBlu;
+        }
+
+        public function registerUserSubscription(int $account_id, int $subscription_id, int $access_record_id): UserSubscription
+        {
+            $account_id = (int)$account_id;
+            $subscription_id = (int)$subscription_id;
+            $access_record_id = (int)$access_record_id;
+            $status = (int)UserSubscriptionStatus::Active;
+
         }
     }
--- src/OpenBlu/OpenBlu.php
+++ src/OpenBlu/OpenBlu.php
@@ -51,6 +51,11 @@
         include_once(__DIR__ . DIRECTORY_SEPARATOR . 'acm' . DIRECTORY_SEPARATOR . 'acm.php');
     }
 
+    if(class_exists('msqg\msqg') == false)
+    {
+        include_once(__DIR__ . DIRECTORY_SEPARATOR . 'msqg' . DIRECTORY_SEPARATOR . 'msqg.php');
+    }
+
     include_once(__DIR__ . DIRECTORY_SEPARATOR . 'AutoConfig.php');
 
     /**
